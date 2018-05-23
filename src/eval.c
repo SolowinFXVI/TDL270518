083e0655f6fdc98a9741ae1adc89d5c5bc39d704
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-23 17:43:27 +0200

    first steps

--- src/eval.c
+++ src/eval.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum type{
+  PARENTHESE = 0,
+  OPERATEUR = 1,
+  ENTIER = 2
+};
+
+typedef struct token {
+  int type;
+  char op;
+  char par;
+  unsigned int val;
+}token;
+
+typedef struct liste_token{
+  token t;
+  liste_token *next;
+}liste_token;
+
+liste_token string_to_token (char *string){
+
+  
+}
 
 int main(int argc, char const *argv[]) {
 
