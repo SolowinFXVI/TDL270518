245a6f5d072ad0f573e99f45d348ec7c6d2faecf
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-24 12:53:40 +0200

    broken

--- src/eval.c
+++ src/eval.c
@@ -17,12 +17,42 @@ typedef struct token {
 
 typedef struct liste_token{
   token t;
-  liste_token *next;
+  struct liste_token *prev;
+  struct liste_token *next;
 }liste_token;
 
 liste_token string_to_token (char *string){
-
-  
+  liste_token* head = NULL;
+  head = (liste_token*) malloc(sizeof(liste_token));
+  int numcount = 0;
+  int val = 0;
+  int i;
+  while(string[i] != '\0'){
+    if((0 <= atoi(string[i])) && (atoi(string[i]) < 10)){
+      numcount++;
+      val = val + atoi(string[i]);
+    }
+    else{
+      if(numcount > 0){
+        liste_token* new_token = (liste_token*) malloc(sizeof(liste_token));
+        new_token->t.type = ENTIER;
+        new_token->t.val = val;
+        numcount = 0;
+        val = 0;
+      }
+      if((string[i] == '(') || (string[i] == ')')){
+        liste_token* new_token = (liste_token*) malloc(sizeof(liste_token));
+        new_token->t.type = PARENTHESE;
+        new_token->t.par = string[i];
+      }
+      if((string[i] =='+')||(string[i] =='-')||(string[i] =='*')||(string[i] =='/')){
+        liste_token* new_token = (liste_token*) malloc(sizeof(liste_token));
+        new_token->t.type = OPERATEUR;
+        new_token->t.op = string[i];
+      }
+    }
+    i++;
+  }
 }
 
 int main(int argc, char const *argv[]) {
