dca346ef4fcf3c6590930c1c69b9e3facac03a87
SolowinFXVI <julien.n.jacquet@gmail.com>
2018-05-26 16:41:12 +0200

    test2 working

--- src/eval.c
+++ src/eval.c
@@ -23,10 +23,15 @@ typedef struct liste_token{
 }liste_token;
 
 typedef struct arbre_token{
-  token t;
+  token data;
   struct arbre_token *left;
   struct arbre_token *right;
 }arbre_token;
+
+typedef struct stack_node{
+  token s;
+  struct stack_node* prev;
+}stack_node;
 
 liste_token* create(token t,liste_token* next){
   liste_token* new_liste_token = (liste_token*)malloc(sizeof(liste_token));
@@ -110,6 +115,38 @@ liste_token* string_to_token (liste_token* head, char *string){
     printtoken(t);
   }
   return head;
+}
+
+void init_stack(stack_node** top){
+  *top = NULL;
+}
+
+stack_node* push(stack_node* top,token s){
+  stack_node* tmp = (stack_node*)malloc(sizeof(stack_node));
+  if(tmp == NULL){
+    perror("Impossible de creer un element de pile");
+    exit(EXIT_FAILURE);
+  }
+  tmp->s = s;
+  tmp->prev = top;
+  top = tmp;
+  return top;
+}
+
+
+stack_node* pop(stack_node *top){
+  stack_node* tmp = top;
+  top = top->prev;
+  free(tmp);
+  return top;
+}
+
+token* top_of_stack(stack_node *top){
+  return &top->s;
+}
+
+int is_empty(stack_node* top){
+  return (top == NULL) ? 1 : 0;
 }
 
 int check_arithm(liste_token *head){
@@ -227,8 +264,146 @@ liste_token* revert(liste_token* head){
   return head;
 }
 
+arbre_token* newNode(liste_token* head){
+  arbre_token* temp = (arbre_token*)malloc(sizeof(arbre_token));
+  if(temp == NULL){
+    perror("impossible de creer un element de l'arbre");
+    exit(0);
+  }
+  temp->left = NULL;
+  temp->right = NULL;
+  temp->data = head->t;
+  return temp;
+}
+
+/*
+arbre_token* maketree(liste_token* head){
+  stack_node *top;
+  arbre_token *t, *t1, *t2;
+  init_stack(&top);
+  while(head != NULL){
+    if(head->t.type == OPERATEUR){
+      t = newNode(head);
+      top = push(top,t->data);
+    }
+    else if(head->t.type == PARENTHESE){
+      if(head->t.type.par == ')'){
+        top = push(top,t->data);
+      }
+      else{
+        while(top->s.type.par != '('){
+          t = newNode()
+          top = pop(top);
+        }
+      }
+    }
+    else if(head->t.type == ENTIER){
+      t1 =
+    }
+
+    head=head->next;
+  }
+  return t;
+}*/
+
+int evaluate(liste_token* head){
+  int final = 0;
+  token val_tok;
+  val_tok.type = ENTIER;
+  val_tok.val = 0;
+  int val = 0;
+  int nbr;
+  stack_node *top;
+  init_stack(&top);
+  while(head != NULL){
+    if(head->t.type == PARENTHESE){
+      if(head->t.par == ')'){
+        top = push(top,head->t);
+      }
+      if(head->t.par == '('){
+        while(top_of_stack(top)->par != ')'){//calcule
+          printf("calcule in \n");
+          printtoken(*top_of_stack(top));
+          if(top_of_stack(top)->type == OPERATEUR){
+            if(top_of_stack(top)->op == '+'){
+              top = pop(top);
+              val = nbr + top_of_stack(top)->val;
+              printf("valtmp + = %d \n",val);
+            }
+            else if(top_of_stack(top)->op == '-'){
+              val = nbr - val;
+              printf("valtmp  - = %d \n",val);
+              /*
+              Incomplet (cas specifique du moins en temps qu'operateur ou signe)
+              */
+            }
+            else if(top_of_stack(top)->op == '*'){
+              val = nbr * val;
+              printf("valtmp  * = %d \n",val);
+            }
+            else if(top_of_stack(top)->op == '/'){
+              val = nbr / val;
+              printf("valtmp / = %d \n",val);
+            }
+          }
+          else if(top_of_stack(top)->type == ENTIER){
+            nbr = top_of_stack(top)->val;
+          }
+          top = pop(top);
+        }
+        top = pop(top);
+        val_tok.val = val;
+        top = push(top,val_tok);
+      }
+    }
+    else{
+      top= push(top,head->t);
+    }
+    //val = 0;
+    head = head->next;
+  }
+  //il faut maintenant finir d'evaluer la pile :
+  while(is_empty(top)== 0){//calcule
+    printf("while calcule final\n");
+    printtoken(*top_of_stack(top));
+    if(top_of_stack(top)->type == OPERATEUR){
+      if(top_of_stack(top)->op == '+'){
+        val = val + nbr;
+        printf("valtmp final + = %d\n",val);
+      }
+      if(top_of_stack(top)->op == '-'){
+        val = val - nbr;
+        printf("valtmp final - = %d\n",val);
+        /*
+        Incomplet (cas specifique du moins en temps qu'operateur ou signe)
+        */
+      }
+      if(top_of_stack(top)->op == '*'){
+        val = val * nbr;
+        printf("valtmp final * = %d\n",val);
+      }
+      if(top_of_stack(top)->op == '/'){
+        val = val / nbr;
+        printf("valtmp final / = %d\n",val);
+      }
+    }
+    else if(top_of_stack(top)->type == ENTIER){
+      nbr = top_of_stack(top)->val;
+    }
+    top = pop(top);
+  }
+  printf("final = %d\n",val);
+  return val;
+}
+
+/*
+int arbre_to_int(arbre_token *at){
+  return 0;
+};*/
+
 int main(int argc, char *argv[]) {
   liste_token* head = NULL;
+  int res = 0;
   if(argc <= 1){
     perror("Equation manquante ?\n");
     exit(EXIT_FAILURE);
@@ -240,6 +415,8 @@ int main(int argc, char *argv[]) {
     perror("Expression non conforme !\n");
     exit(EXIT_FAILURE);
   }
+  res = evaluate(head);
   destroy(head);//detruit la liste
+  printf("Fin.\n");
   return 0;
 }
